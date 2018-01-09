af04fde18cf8fbffa0d2328c017623f6461809bc
bkushigian <bkushigian@gmail.com>
2018-01-09 09:00:01 -0500

    Implemented tests for arrayops.py

--- agt/util/test/test_arrayops.py
+++ agt/util/test/test_arrayops.py
@@ -5,16 +5,43 @@ from agt.util.arrayops import (zero, one, index, ones, diagonal,
 
 class TestArrayOps(TestCase):
     def test_index(self):
-        self.fail()
+        l = list(range(10))
+        for i in l:
+            self.assertEqual(i, index(l, i))
 
     def test_ones(self):
-        self.fail()
+        M = ones((3, 3))
+        self.assertEqual((3,3), M.shape)
+        for row in M:
+            for x in row:
+                self.assertEqual(one, x)
 
     def test_diagonal(self):
-        self.fail()
+        M = diagonal(3)
+        self.assertEqual((3, 3), M.shape)
+        for i, row in enumerate(M):
+            for j, x in enumerate(row):
+                if i == j:
+                    self.assertEqual(one, x)
+                else:
+                    self.assertEqual(zero, x)
 
     def test_dense_adjacency_matrix(self):
-        self.fail()
+        M = dense_adjacency_matrix(3)
+        for i, row in enumerate(M):
+            for j, x in enumerate(row):
+                if i == j:
+                    self.assertEqual(0, x)
+                else:
+                    self.assertEqual(1, x)
 
     def generate_lower_triangle(self):
-        self.fail()
+        indices = set(generate_lower_triangle(3))
+        expected = {(1, 0), (2, 0), (2, 1)}
+        self.assertEqual(expected, indices)
+
+    def generate_lower_triangle_with_diagonal(self):
+        indices = set(generate_lower_triangle(3, diagonal=True))
+        expected = {(0, 0), (1, 0), (1, 1), (2, 0), (2, 1), (2, 2)}
+        self.assertEqual(expected, indices)
+
