61501713935e54a1eac99654eca2bf8f84af838a
bkushigian <bkushigian@gmail.com>
2018-01-09 08:37:58 -0500

    Created test files for arraypos

---
+++ agt/util/test/test_arrayops.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+from agt.util.arrayops import (zero, one, index, ones, diagonal,
+                               dense_adjacency_matrix, generate_lower_triangle)
+
+
+class TestArrayOps(TestCase):
+    def test_index(self):
+        self.fail()
+
+    def test_ones(self):
+        self.fail()
+
+    def test_diagonal(self):
+        self.fail()
+
+    def test_dense_adjacency_matrix(self):
+        self.fail()
+
+    def generate_lower_triangle(self):
+        self.fail()
